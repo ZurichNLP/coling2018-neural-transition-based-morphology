2b45e09de5802c65cbfa7478c62168e9caac02ba
Simon Clematide <simon.clematide@uzh.ch>
2018-06-27 15:25:14 +0200

    adapt sigmorphon2016 config

--- dataset_configs/sigmorphon2016_config.py
+++ dataset_configs/sigmorphon2016_config.py
@@ -4,7 +4,7 @@ SEEDS = [1, 2, 3, 4, 5]
 
 
 RESULTSDIR = '../../paper2018/results/sigmorphon2016'
-#RESULTSDIR = '/mnt/storage/bender/projects/cl/sigmorphon-uzh/paper2018/results/sigmorphon2016'
+
 
 
 DATASET_SHORTNAME = 'sigmorphon2016'
@@ -12,16 +12,16 @@ DATASET_SHORTNAME = 'sigmorphon2016'
 DATASET_PATH = '../data/sigmorphon2016/data/'
 
 DATASET_FOLDS = [
-#'arabic-task1',
-#'finnish-task1',
-#'georgian-task1',
-#'german-task1',
-#'hungarian-task1',
+'arabic-task1',
+'finnish-task1',
+'georgian-task1',
+'german-task1',
+'hungarian-task1',
 'maltese-task1',
-#'navajo-task1',
-#'russian-task1',
-#'spanish-task1',
-#'turkish-task1'
+'navajo-task1',
+'russian-task1',
+'spanish-task1',
+'turkish-task1'
 ]
 
 
@@ -34,9 +34,6 @@ DATASET_TESTFILE_NAME = '-test'
 
 MODEL_CONFIGS = [('hard', '', 'hard'),
                  ('haem', '', 'haem'),
-                 #('hacm', '', 'hacm'),
-                 #('haem', '--substitution', 'haem_sub'),
-                 #('hacm', '--copy-as-substitution', 'hacm_sub')
                 ]
 
 # the last element in a tuple is the string that goes into the results directory name
@@ -49,17 +46,9 @@ ALIGN_CONFIGS = [('--align-smart', 'crp'),
 
 # the last element in a tuple is the string that goes into the results directory name
 
-MODE_CONFIGS = [#('mle', '', ''),
-                # ('rl', 'DUMMY', ''),
-                # ('mrt', 'DUMMY', ''),
-                #('ss', '', '', 5, 5),
+MODE_CONFIGS = [('mle', '', ''),
                 ('mrt', '--alpha=1', '1', 5, 5),
-                #models.dynamic_oracles(5, 5),
-                #models.lols(5, 5),
-                #('ss', '--ss-optimal-oracle --ss-loss=nll --ss-beta=1 --ss-global-rollout --ss-pretrain-epochs=10', 'di', 5, 5),
-                #('ss', '--ss-optimal-oracle --ss-loss=nll --ss-beta=0.5 --ss-global-rollout --ss-pretrain-epochs=10', 'roi', 5, 5),
-                #models.lols_margin(5, 5),
-                #('ss', '--ss-optimal-oracle --ss-loss=softmax-margin --ss-beta=0.5 --ss-global-rollout --ss-pretrain-epochs=10', 'roim', 5, 5),
+
                ]
 
 
@@ -71,7 +60,7 @@ MORE_PARAMS = dict(
     #PRETRAIN = '--pretrain-epochs=5',
     # DATA
     DATAFORMAT = '',  # no --sigm2017format flag
-    POSEMB     = '--pos-emb',  #  '--avm-feat-format',   
+    POSEMB     = '--pos-emb',  #  '--avm-feat-format',
     # DECODING
     BEAMWIDTHS   = '4'
 )
